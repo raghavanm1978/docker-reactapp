06576995aa18b8d690fccd3d82b4e9e364c678e8
Raghavan <raghavan.mohan@gmail.com>
2021-06-03 22:21:23 -0400

    added learn react to make test success

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+        learn react
          This is active reload yay!!!!
         </a>
       </header>
